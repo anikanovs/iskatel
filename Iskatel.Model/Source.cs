3d55bfca7bf825a502efc4d97ed7d918853edc04
Sergey Anikanov <anikanovs@yandex.ru>
2016-12-11 23:08:57 +0300

    test commit

--- Iskatel.Model/Source.cs
+++ Iskatel.Model/Source.cs
@@ -5,9 +5,6 @@ namespace Iskatel.Model
     public class Source
     {
         public string Title { get; set; }
-        public List<Person> Authors { get; set; }
         public Date CreateDate { get; set; }
-        public List<string> Genres { get; set; }
-        public List<Human> Characters { get; set; }
     }
 }
